829295684c81f0e6a0e7723228e284b2346c9c84
user <user@DESKTOP-TN8GGFQ>
2020-03-19 11:00:42 +0900

    Create a menu

---
+++ src/HelloWorld.java
@@ -0,0 +1,16 @@
+
+public class HelloWorld {
+	
+	public static void main(String[] args) {
+		
+		System.out.println(">>> News Article Management System Menu <<<");
+		System.out.println("1. Add Article Headline");
+		System.out.println("2. Delete Article");
+		System.out.println("3. Edit Article");
+		System.out.println("4. View All Article");
+		System.out.println("5. View Favorite Article");
+		System.out.println("6. Exit");
+		
+	
+	}
+}
