6e9033bd17f1100104feddf797081512ec56e248
user <user@DESKTOP-TN8GGFQ>
2020-03-20 20:20:37 +0900

    something adjust

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -1,9 +1,9 @@
 
-public class HelloWorld {
+public class MenuManager {
 	
 	public static void main(String[] args) {
 		
-		System.out.println(">>> News Article Management System Menu <<<");
+		System.out.println(">>> Welcome! News Article Management System Menu <<<");
 		System.out.println("1. Add Article Headline");
 		System.out.println("2. Delete Article");
 		System.out.println("3. Edit Article");
