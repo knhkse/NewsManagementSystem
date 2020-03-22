0e0bb2c185db400299d4516eadbf158c7e918b8c
KNH <user@DESKTOP-TN8GGFQ>
2020-03-22 11:34:50 +0900

    some adjustment

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -3,13 +3,14 @@ public class MenuManager {
 	
 	public static void main(String[] args) {
 		
-		System.out.println(">>> News Article Management System Menu <<<");
-		System.out.println("1. Add Article Headline");
+		System.out.println(">>> News Article Management System Menu <<<\n");
+		System.out.println("1. Add News Headline");
 		System.out.println("2. Delete Article");
 		System.out.println("3. Edit Article");
-		System.out.println("4. View All Article");
+		System.out.println("4. View All Article List");
 		System.out.println("5. View Favorite Article");
-		System.out.println("6. Exit");
+		System.out.println("6. View Article Category");
+		System.out.println("7. Exit");
 		
 	
 	}
